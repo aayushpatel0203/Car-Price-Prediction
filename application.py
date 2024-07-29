d5d691557b702227182f836a9dff5cc87d99a2f3
Aayush_Patel <146657049+aayushpatel0203@users.noreply.github.com>
2024-07-29 18:35:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 # Load the model and data with error handling
 try:
